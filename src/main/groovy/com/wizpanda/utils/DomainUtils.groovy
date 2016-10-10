09fa313b8d026e60efa383fe8e0d0233d1ea58f2
Shashank Agrawal <shagrawal99@gmail.com>
2016-10-10 17:22:32 +0530

    Added save method to support Grails 3.2.0 as well.

--- src/main/groovy/com/wizpanda/utils/DomainUtils.groovy
+++ src/main/groovy/com/wizpanda/utils/DomainUtils.groovy
@@ -1,6 +1,8 @@
 package com.wizpanda.utils
 
 import org.apache.commons.logging.Log
+import org.grails.datastore.gorm.GormEntity
+import org.slf4j.Logger
 
 /**
  * @author Shashank Agrawal
@@ -8,7 +10,22 @@ import org.apache.commons.logging.Log
  */
 class DomainUtils {
 
-    static boolean save(Object domainInstance, boolean flush, Log log) {
+    /**
+     * This is to support older version of Grails before 3.2.0 since the log property injected at compile time
+     * into all classes is now an Slf4j Logger instance and not an instance of the Commons Logging Log class
+     */
+    static boolean save(GormEntity domainInstance, boolean flush, Log log) {
+        _save(domainInstance, flush, log)
+    }
+
+    /**
+     * This is to support Grails version 3.2.0 or higher since the SLF4J is now default logger in Grails 3.2.0
+     */
+    static boolean save(GormEntity domainInstance, boolean flush, Logger log) {
+        _save(domainInstance, flush, log)
+    }
+
+    static private _save(GormEntity domainInstance, boolean flush, def log) {
         if (!domainInstance) {
             return false
         }
