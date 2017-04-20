2beaba388136cad6e0673a64a16365d4aef0a6f3
Shashank Agrawal <shagrawal99@gmail.com>
2017-04-20 19:23:37 +0530

    Adding some more exceptions.

--- grails-app/controllers/com/wizpanda/core/CommonExceptionHandler.groovy
+++ grails-app/controllers/com/wizpanda/core/CommonExceptionHandler.groovy
@@ -1,13 +1,34 @@
 package com.wizpanda.core
 
+import com.wizpanda.exception.InvalidDataException
 import com.wizpanda.exception.NotAcceptableException
+import com.wizpanda.exception.NotFoundException
+import com.wizpanda.exception.OperationFailedException
+import grails.converters.JSON
 import org.springframework.http.HttpStatus
 
 trait CommonExceptionHandler {
 
     static responseFormats = ["json"]
 
+    private def respondException(Exception e, HttpStatus status) {
+        response.status = status.value()
+        render([errors: [[message: e.message, severity: "error", ttl: 10000]]] as JSON)
+    }
+
     def handleInvalidDataException(NotAcceptableException e) {
-        respond([status: HttpStatus.NOT_ACCEPTABLE], [errors: [[message: e.message, severity: "error", ttl: 10000]]])
+        respondException(e, HttpStatus.NOT_ACCEPTABLE)
+    }
+
+    def handleInvalidDataException(InvalidDataException e) {
+        respondException(e, HttpStatus.NOT_ACCEPTABLE)
+    }
+
+    def handleOperationFailedException(OperationFailedException e) {
+        respondException(e, HttpStatus.NOT_ACCEPTABLE)
+    }
+
+    def handleNotFoundException(NotFoundException e) {
+        respondException(e, HttpStatus.NOT_FOUND)
     }
 }
---
+++ src/main/groovy/com/wizpanda/exception/OperationFailedException.groovy
@@ -0,0 +1,13 @@
+package com.wizpanda.exception
+
+/**
+ * Used to throw when an operation failed because of some exception or some falsy data.
+ * 
+ * @author Shashank Agrawal
+ */
+class OperationFailedException extends Exception {
+
+    OperationFailedException(String message = "") {
+        super(message)
+    }
+}
