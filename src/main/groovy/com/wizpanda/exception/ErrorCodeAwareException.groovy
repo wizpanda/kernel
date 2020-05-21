cc70d87ff332ef6f6f91b91059e2e57743422ef7
Shashank Agrawal <shashank@wizpanda.com>
2020-05-21 17:48:17 +0530

    Added httpResponseCode in ErrorCodeAwareException

--- src/main/groovy/com/wizpanda/exception/ErrorCodeAwareException.groovy
+++ src/main/groovy/com/wizpanda/exception/ErrorCodeAwareException.groovy
@@ -4,6 +4,7 @@ abstract class ErrorCodeAwareException extends RuntimeException {
 
     Integer errorCode
     Map additionalData
+    Integer httpResponseCode
 
     ErrorCodeAwareException(String message = null, Throwable cause = null) {
         super(message, cause)
